c070d272a5cefa6014ee6a97e50d83f3bf958f35
Pedrinho Hilário <flygames1214@gmail.com>
2025-05-12 15:42:00 -0300

    Criando component de icone personalizado

--- components/ui/IconProp.tsx
+++ components/ui/IconProp.tsx
@@ -1,2 +1,14 @@
 import React from "react";
 import { IconProps } from "@/types/IIconProps";
+
+export function IconProp({ size, src, alt="", className = '' }: IconProps) {
+    const imageSrc = src.startsWith('/') ? src : `${src}`;
+
+    return(
+        <img 
+        src={imageSrc} 
+        alt={alt}
+        className={`object-contain w-[${size}px] h-[${size}px] ${className}`}
+         />
+    )
+}
